5dcfb59fe67f21c884d6e8ca17b5afe0082597ac
merwyn-c <merwyn.carrillos@fox.com>
2024-05-15 21:31:49 -0400

    delete jinja app file, it's not needed anymore.

--- zendesk-time-tracker/main.py
+++ zendesk-time-tracker/main.py
@@ -1,21 +1,38 @@
+import platform
+import sys
 import eel
-from timeit import timeit
-import os, random
 
-eel.init('site')
+eel.init('app')  # Give folder containing app files
 
 
 @eel.expose
-def pick_file(folder):
-    if os.path.isdir(folder):
-        return random.choice(os.listdir(folder))
-    else:
-        return 'Not valid folder'
+def py_random():
+    return 5
 
 
-@eel.expose
-def start_timer(organization):
-    timeit()
+@eel.expose  # Expose this function to Javascript
+def print_console(x):
+    return f'Hello from {x}'
 
 
-eel.start('index.html', size=(400, 120))
+def start_eel():
+    page = 'templates/hello.html'
+    eel_kwargs = dict(
+        host='localhost',
+        port=8080,
+        size=(400, 300),
+        jinja_templates='templates'
+    )
+
+    try:
+        eel.start(page, **eel_kwargs)  # Start
+    except EnvironmentError:
+        # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
+        if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
+            eel.start(page, mode='edge', **eel_kwargs)
+        else:
+            raise
+
+
+if __name__ == '__main__':
+    start_eel()
