10174748f9ae94bf7d84082a34c0962e2444cf36
Merwyn Carrillos <merwyn.ca@gmail.com>
2024-05-22 22:33:49 -0400

    start time updates

--- zendesk-time-tracker/update.py
+++ zendesk-time-tracker/update.py
@@ -1,6 +1,5 @@
 import eel
-from time import strftime, localtime
-import datetime
+from datetime import datetime
 import time
 import file
 
@@ -12,20 +11,33 @@ def active_button(name: str, obj_type: str = "organization"):
 
 def start_time(name: str, exec_time: int, obj_type: str = "organization"):
     """
-
+    Update a start time for an activated button
     :param name:
     :param exec_time: datetime of when to add a thing.
     :param obj_type:
     :return:
     """
-    data = file.read_json(filename="history.json")
-    print(data)
+    all_data = file.read_json(filename="history.json")
+
+    data = all_data[obj_type]
+    date = str(datetime.utcfromtimestamp(exec_time).date())
+
+    # Make sure a day is created for this tracking.
+    if not data.get(date):
+        data[date] = {name: {str(exec_time): None}}
+    elif not data[date].get(name):
+        data[date][name] = {str(exec_time): None}
+    else:
+        data[date][name].update({str(exec_time): None})
+
+    all_data[obj_type] = data
+
+    file.write_json(filename="history.json", data=all_data)
 
 
 if __name__ == "__main__":
-    print(int(time.time()))
+    time_now = int(time.time())
     sample_time = 1716429843
-    dt = datetime.datetime.utcfromtimestamp(sample_time)
-    date = dt.date()
-    print(date)
-    start_time(name="metallica", exec_time=0)
+
+    start_time(name="metallica", exec_time=sample_time)
+    start_time(name="metallica", exec_time=time_now)
